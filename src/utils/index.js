9178800417f4223d8212a9852f051ee171389a73
Jason Arthur <jason.arthur91@gmail.com>
2023-06-08 13:53:04 +0700

    postgres database implemented

---
+++ src/utils/index.js
@@ -0,0 +1,21 @@
+/* eslint-disable linebreak-style */
+/* eslint-disable eol-last */
+/* eslint-disable camelcase */
+/* eslint-disable linebreak-style */
+const mapDBToModel = ({
+  id,
+  title,
+  body,
+  tags,
+  created_at,
+  updated_at,
+}) => ({
+  id,
+  title,
+  body,
+  tags,
+  createdAt: created_at,
+  updatedAt: updated_at,
+});
+
+module.exports = { mapDBToModel };
